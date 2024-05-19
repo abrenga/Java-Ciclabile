4bb36bf160c2a951353fed7da1e1d9ae93591fb9
a.brenga <antobrg90@hotmail.it>
2024-05-20 00:38:41 +0200

    started the Part4 of Mileston2

--- src/org/java/generate/ProgrammEventi.java
+++ src/org/java/generate/ProgrammEventi.java
@@ -1,22 +1,47 @@
 package org.java.generate;
 
-public class ProgrammEventi {
-/*Creare una classe ProgrammEventi con i seguenti attributi: 
-- titolo: String
-- eventi: List<Evento>
-Nel costruttore valorizzare il titolo, passato come parametro, e inizializzare la lista di eventi come una nuova ArrayList
-Aggiungere i seguenti metodi :
-un metodo che aggiunge alla lista un Evento, passato come parametro
-un metodo che restituisce una lista con tutti gli eventi presenti in una certa data
-un metodo che restituisce quanti eventi sono presenti nel programma
-un metodo che svuota la lista di eventi
-un metodo che restituisce una stringa che mostra il titolo del programma e tutti gli eventi ordinati per data nella forma: 
-data1 - titolo1
-data2 - titolo2 
-*/
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProgrammEventi {
+
+	private String titolo;
+	private List<Evento> eventi;
+
+	public ProgrammEventi(String titolo) {
+		this.titolo = titolo;
+		this.eventi = new ArrayList<Evento>();
+
+	}
+
+//un metodo che aggiunge alla lista un Evento, passato come parametro
+	public void addEvento(Evento evento) {
+		eventi.add(evento);
+
+	}
+
+	// un metodo che restituisce una lista con tutti gli eventi presenti in una
+	// certa data
+	public List<Evento> listaDiEventi(LocalDate data) {
+		List<Evento> eventoPerData= new ArrayList<Evento>(); 
+
+		for (int i = 0; i < eventi.size(); i++) {
+			if (data.equals(eventi.get(i).getDataConcerto())) {
+				eventoPerData.add(eventi.get(i));	
+				
+			}
+			
+		}
+		return eventoPerData;
+	}
+	
+	public void removeEvento() {	
+		eventi.clear();
+		
+	}
 	
 	
-	
+	//un metodo che restituisce quanti eventi sono presenti nel programma
 
 }
